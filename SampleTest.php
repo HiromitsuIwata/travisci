6ab7eaad481d62a1092849d58e7eaf060b86aeba
IwataHiromitsu <h.iwata@grooo.co.jp>
2015-09-16 14:02:09 +0900

    modify SampleTest.php

--- SampleTest.php
+++ SampleTest.php
@@ -14,7 +14,7 @@ class SampleTest extends PHPUnit_Framework_TestCase
     public function aaa()
     {
     	$Sample = new Sample();
-        $this->assertEquals($Sample->helloWorld(), 'aaaaa');
+        $this->assertEquals($Sample->helloWorld(), 'hello world!');
     }
 }
 
